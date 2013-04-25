28cbe9898e9bf7c098e3387cca13f2c62aa84e65
Florian Mounier <florian.mounier@kozea.fr>
2013-04-25 10:41:18 +0200

    And... it's work

--- wdb/__main__.py
+++ wdb/__main__.py
@@ -1,10 +1,11 @@
 import sys
 import os
-from wdb import WdbRequest, AltServer
-from random import randint
+from bdb import BdbQuit
+from wdb import Wdb
 
 
 def main():
+    """Inspired by python -m pdb. Debug any python script with wdb"""
     if not sys.argv[1:] or sys.argv[1] in ("--help", "-h"):
         print "usage: wdb.py scriptfile [arg] ..."
         sys.exit(2)
@@ -17,22 +18,19 @@ def main():
     del sys.argv[0]
     sys.path[0] = os.path.dirname(mainpyfile)
 
-    rand_ports = [randint(10000, 60000) for _ in range(5)]
-    AltServer(rand_ports)
-    self = WdbRequest(rand_ports)
+    # Let's make a server in case of
+    wdbr = Wdb.make_server()
 
-    self.quitting = 0
-    self.begun = False
-    self.reset()
-
+    # Prepare full tracing
     frame = sys._getframe()
     while frame:
-        frame.f_trace = self.trace_dispatch
-        self.botframe = frame
+        frame.f_trace = wdbr.trace_dispatch
+        wdbr.botframe = frame
         frame = frame.f_back
-    self.stopframe = sys._getframe().f_back
-    self.stoplineno = -1
-    sys.settrace(self.trace_dispatch)
+    wdbr.stopframe = sys._getframe().f_back
+    wdbr.stoplineno = -1
+
+    # Init the python context
     import __main__
     __main__.__dict__.clear()
     __main__.__dict__.update({
@@ -40,10 +38,17 @@ def main():
         "__file__": mainpyfile,
         "__builtins__": __builtins__,
     })
-    self._wait_for_mainpyfile = 1
-    self.mainpyfile = self.canonic(mainpyfile)
-    statement = 'execfile(%r)' % mainpyfile
-    self.run(statement)
+    cmd = 'execfile(%r)\n' % mainpyfile
+
+    # Set trace with wdb
+    sys.settrace(wdbr.trace_dispatch)
+    try:
+        exec cmd in __main__.__dict__, __main__.__dict__
+    except BdbQuit:
+        pass
+    finally:
+        wdbr.quitting = 1
+        sys.settrace(None)
 
 
 if __name__ == '__main__':
