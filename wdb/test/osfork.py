14e3a38ccc61252a850ae6543c9739d242eb7505
Florian Mounier <florian.mounier@kozea.fr>
2013-04-29 11:40:30 +0200

    Add os fork test

---
+++ wdb/test/osfork.py
@@ -0,0 +1,16 @@
+import os
+
+print 'Forking'
+
+pid = os.fork()
+
+if pid == 0:
+    print 'In children'
+    import wdb; wdb.set_trace()
+    print 'Children dead'
+else:
+    print 'In parent'
+    import wdb; wdb.set_trace()
+    print 'Parent dead'
+
+print 'The End'
