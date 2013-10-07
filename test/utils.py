6f3e83da09f74605bdde452ede22f899d299a83b
Florian Mounier <florian.mounier@kozea.fr>
2013-10-07 14:43:30 +0200

    Fix pypy

--- test/utils.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-python_version = sys.version_info[0]
-
-if python_version == 2:
-    division_by_zero_message = 'integer division or modulo by zero'
-elif python_version == 3:
-    division_by_zero_message = 'division by zero'
