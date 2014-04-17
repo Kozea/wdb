3047101b4bcd55bbee0295b204e32e370b65b45c
Florian Mounier <florian.mounier@kozea.fr>
2014-04-17 09:58:53 +0200

    Now we need a connection even though there's no interaction

--- test/test_no_error.py
+++ test/test_no_error.py
@@ -2,12 +2,6 @@
 from .conftest import use
 
 
-@use('error_ignored_in_script.py')
-def test_with_error_ignored(socket):
-    # If it doesn't timeout this is good
-    socket.join()
-
-
 @use('error_not_ignored_in_script.py')
 def test_with_error_not_ignored_because_of_full(socket):
     socket.start()
