90b471e1c626ad0fb09bec5728f0b493f793abfa
Florian Mounier <florian.mounier@kozea.fr>
2016-05-09 13:10:20 +0200

    Remove stdout switch at start, fix pytest_wdb

--- pytest_wdb/test_wdb.py
+++ pytest_wdb/test_wdb.py
@@ -44,12 +44,12 @@ class FakeWdbServer(Process):
 
 
 def test_ok(testdir):
-    testdir.makepyfile('''
+    p = testdir.makepyfile('''
         def test_run():
             print('Test has been run')
     ''')
     with FakeWdbServer():
-        result = testdir.runpytest('--wdb')
+        result = testdir.runpytest_inprocess('--wdb', p)
     result.stdout.fnmatch_lines([
         'plugins:*wdb*'
     ])
@@ -57,13 +57,13 @@ def test_ok(testdir):
 
 
 def test_ok_run_once(testdir):
-    testdir.makepyfile('''
+    p = testdir.makepyfile('''
         def test_run():
             print('Test has been run')
     ''')
 
     with FakeWdbServer():
-        result = testdir.runpytest('--wdb', '-s')
+        result = testdir.runpytest_inprocess('--wdb', '-s', p)
 
     assert len([line for line in result.stdout.lines
                 if line == 'test_ok_run_once.py Test has been run']) == 1
@@ -73,26 +73,26 @@ def test_ok_run_once(testdir):
 # Todo implement fake wdb server
 
 def test_fail_run_once(testdir):
-    testdir.makepyfile('''
+    p = testdir.makepyfile('''
         def test_run():
             print('Test has been run')
             assert 0
     ''')
     with FakeWdbServer(stops=True):
-        result = testdir.runpytest('--wdb', '-s')
+        result = testdir.runpytest_inprocess('--wdb', '-s', p)
     assert len([line for line in result.stdout.lines
                 if line == 'test_fail_run_once.py Test has been run']) == 1
     assert result.ret == 1
 
 
 def test_error_run_once(testdir):
-    testdir.makepyfile('''
+    p = testdir.makepyfile('''
         def test_run():
             print('Test has been run')
             1/0
     ''')
     with FakeWdbServer(stops=True):
-        result = testdir.runpytest('--wdb', '-s')
+        result = testdir.runpytest_inprocess('--wdb', '-s', p)
     assert len([line for line in result.stdout.lines
                 if line == 'test_error_run_once.py Test has been run']) == 1
     assert result.ret == 1
