295761e737cb32503b53f773ee1830201f5b6504
Florian Mounier <florian.mounier@kozea.fr>
2016-06-24 11:56:47 +0200

    Bump Version 3.0.4 -> 3.0.5

--- server/coffees/wdb.coffee
+++ server/coffees/wdb.coffee
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 class Wdb extends Log
-  __version__: '3.0.4'
+  __version__: '3.0.5'
 
   constructor: ->
     super
--- server/wdb_server/static/javascripts/wdb.js
+++ server/wdb_server/static/javascripts/wdb.js
@@ -1295,7 +1295,7 @@ help = "<div class=\"mdl-tabs mdl-js-tabs mdl-js-ripple-effect\">\n  <div class=
 Wdb = (function(superClass) {
   extend(Wdb, superClass);
 
-  Wdb.prototype.__version__ = '3.0.4';
+  Wdb.prototype.__version__ = '3.0.5';
 
   function Wdb() {
     Wdb.__super__.constructor.apply(this, arguments);
