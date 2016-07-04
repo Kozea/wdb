d63d922dc1fddfd72559529f08ac5eda874c00c4
Florian Mounier <florian.mounier@kozea.fr>
2016-07-04 11:26:16 +0200

    Add wdb as requirement

---
+++ pytest_wdb/CHANGELOG.md
@@ -0,0 +1,4 @@
+0.4.0
+=====
+
+  * wdb seems like a good install requirement!
--- pytest_wdb/setup.py
+++ pytest_wdb/setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name="pytest_wdb",
-    version='0.3.0',
+    version='0.4.0',
     author="Florian Mounier @ kozea",
     author_email="florian.mounier@kozea.fr",
     url="http://github.com/Kozea/wdb",
     license='GPLv3',
     py_modules=['pytest_wdb'],
+    install_requires=['wdb'],
     description="Trace pytest tests with wdb to halt on error with --wdb.",
     entry_points={
         'pytest11': [
