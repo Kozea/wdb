ed115f1be87c514ee5732638b5aea9d9044a66d2
Florian Mounier <florian.mounier@kozea.fr>
2017-01-13 10:54:19 +0100

    Add flask wdb project to replace werkzeug debugger with a hack

---
+++ flask-wdb/setup.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from setuptools import setup
+from distutils.sysconfig import get_python_lib
+
+site_packages_path = get_python_lib().replace(sys.prefix + os.path.sep, '')
+
+setup(
+    name="flask-wdb",
+    version='0.1.0',
+    author="Florian Mounier @ kozea",
+    author_email="florian.mounier@kozea.fr",
+    url="http://github.com/Kozea/wdb",
+    license='GPLv3',
+    install_requires=['wdb', 'werkzeug'],
+    data_files=[(site_packages_path, ['flask-wdb.pth'])],
+    description="Replace flask werkzeug debugger with wdb."
+)
