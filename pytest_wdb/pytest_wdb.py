d5b41861603c7fd742650dfcdd89e9587263d538
Florian Mounier <paradoxxx.zero@gmail.com>
2019-06-13 12:05:23 +0200

    Rewrite pytest_wdb with a simpler hack

--- pytest_wdb/pytest_wdb.py
+++ pytest_wdb/pytest_wdb.py
@@ -11,24 +11,12 @@ def pytest_addoption(parser):
 
 
 def pytest_configure(config):
-    if config.getoption('--wdb'):
+    if config.option.wdb:
         config.pluginmanager.register(Trace(), '_wdb')
         config.pluginmanager.unregister(name='pdb')
 
 
 class Trace(object):
-    def pytest_pyfunc_call(self, pyfuncitem):
-        testfunction = pyfuncitem.obj
-        if pyfuncitem._isyieldedfunction():
-            with wdb.trace():
-                testfunction(*pyfuncitem._args)
-        else:
-            funcargs = pyfuncitem.funcargs
-            testargs = {}
-            for arg in pyfuncitem._fixtureinfo.argnames:
-                testargs[arg] = funcargs[arg]
-            with wdb.trace():
-                testfunction(**testargs)
-
-        # Avoid multiple test call
-        return True
+    def pytest_collection_modifyitems(config, items):
+        for item in items:
+            item.obj = wdb.with_trace(item.obj)
